dc4e2c8230970bd4bebbfe559287c3c9d374ab3a
Charith Lakshan <it23841086@my.sliit.lk>
2025-06-21 21:58:24 +0530

    Update index.js
    updated

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ function search()
     //OMDb API key = 2329d79a
 
     let htmlRequest = new XMLHttpRequest()
-    
+
     let url = "https://www.omdbapi.com/?apikey=2329d79a&t=" + encodeURIComponent(movieName);
+
+    htmlRequest.open("GET", url)
+
+    htmlRequest.send()
+
+    htmlRequest.responseType = "json"
+
+    htmlRequest.onload = ()=>{
+        let response = htmlRequest.response
+        console.log(response)
+
+        
 
 }
