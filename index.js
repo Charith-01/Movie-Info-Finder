fd46789ea6a3f312ab5b68e580ef7323686d8341
Charith Lakshan <it23841086@my.sliit.lk>
2025-06-21 22:00:49 +0530

    Update index.js
    updated js

--- index.js
+++ index.js
@@ -23,5 +23,26 @@ function search()
         imgTag.src = response.Poster
 
         document.getElementById("title").textContent = response.Title
+        document.getElementById("year").textContent = response.Year
+        document.getElementById("rated").textContent = response.Rated
+        document.getElementById("released").textContent = response.Released
+        document.getElementById("runtime").textContent = response.Runtime
+        document.getElementById("genre").textContent = response.Genre
+        document.getElementById("director").textContent = response.Director
+        document.getElementById("writer").textContent = response.Writer
+        document.getElementById("actors").textContent = response.Actors
+        document.getElementById("plot").textContent = response.Plot
+        document.getElementById("language").textContent = response.Language
+        document.getElementById("country").textContent = response.Country
+        document.getElementById("awards").textContent = response.Awards
+        document.getElementById("boxoffice").textContent = response.BoxOffice
+        document.getElementById("production").textContent = response.Production
+        document.getElementById("dvd").textContent = response.DVD
+        document.getElementById("type").textContent = response.Type
+        document.getElementById("website").textContent = response.Website
+        document.getElementById("rating").textContent = response.imdbRating
+        document.getElementById("votes").textContent = response.imdbVotes
+        document.getElementById("metascore").textContent = response.Metascore
+    }
 
 }
